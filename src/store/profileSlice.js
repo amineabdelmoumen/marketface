ba8438d105a27e7288f075d84a6c74963bbfc005
amine <abdelmoumen33330@gmail.com>
2022-03-17 15:36:32 +0100

    remo

--- src/store/profileSlice.js
+++ src/store/profileSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import {saveReferences} from "../lib/crud";
+import { saveReferences } from "../lib/crud";
 
 const profileSlice = createSlice({
   name: "profile",
@@ -12,93 +12,114 @@ const profileSlice = createSlice({
       nombre_employes: "De 1 à 10",
       region: "Casablanca-Settat",
       ville: "Tanger-Assilah",
-      chiffre_affaire: "< 10 MDhs"
+      chiffre_affaire: "< 10 MDhs",
     },
     marque: {
-      titre: '',
-      annee: '',
-      description: '',
-      categorie: 'Produits chimiques',
-      nom_client: '',
+      titre: "",
+      annee: "",
+      description: "",
+      categorie: "Produits chimiques",
+      nom_client: "",
       images: [],
-      logo: null
+      logo: null,
     },
     references: [],
     catalogue: {
-      vous_etes: 'franchisé',
+      vous_etes: "franchisé",
       matiere: [],
       business: [],
       location: [],
       moyen: [],
       type_vente: [],
       produit_achete: [],
-      distribution: []
+      distribution: [],
     },
     articles: [],
     article: {
       type_article: "Produit",
-      type: "Produits chimiques"
+      type: "Produits chimiques",
     },
     cible: {
-      cherche: "Clients"
+      cherche: "Clients",
     },
     register: {
-      titre: 'm',
-      poste: 'directeur_general'
+      titre: "m",
+      poste: "directeur_general",
+      terms: false,
     },
   },
   reducers: {
     setProfil: (state, action) => {
-      state.identite = {...action.payload}
-      const cible = {...action.payload.cibles[0]}
-      if(Object.keys(cible).length) {
-        state.cible = {...cible}
-        state.cible.regions = cible.regions.split(', ')
-        state.cible.activites = cible.activites.split(', ')
+      state.identite = { ...action.payload };
+      const cible = { ...action.payload.cibles[0] };
+      if (Object.keys(cible).length) {
+        state.cible = { ...cible };
+        state.cible.regions = cible.regions.split(", ");
+        state.cible.activites = cible.activites.split(", ");
       }
-      state.articles = [...action.payload.articles]
-      state.references = [...action.payload.references]
-      const catalogue = {...action.payload.catalogues[0]}
-      if(Object.keys(catalogue).length) {
+      state.articles = [...action.payload.articles];
+      state.references = [...action.payload.references];
+      const catalogue = { ...action.payload.catalogues[0] };
+      if (Object.keys(catalogue).length) {
         state.catalogue = {
           id: catalogue.id,
           vous_etes: catalogue.vous_etes,
-          matiere: catalogue.matiere.length ? catalogue.matiere.split(',') : [],
-          business: catalogue.business.length ? catalogue.business.split(',') : [],
-          location: catalogue.location.length ? catalogue.location.split(',') : [],
-          moyen: catalogue.moyen.length ? catalogue.moyen.split(',') : [],
-          type_vente: catalogue.type_vente.length ? catalogue.type_vente.split(',') : [],
-          produit_achete: catalogue.produit_achete.length ? catalogue.produit_achete.split(',') : [],
-          distribution: catalogue.distribution.length ? catalogue.distribution.split(',') : []
-        }
+          matiere: catalogue.matiere.length ? catalogue.matiere.split(",") : [],
+          business: catalogue.business.length
+            ? catalogue.business.split(",")
+            : [],
+          location: catalogue.location.length
+            ? catalogue.location.split(",")
+            : [],
+          moyen: catalogue.moyen.length ? catalogue.moyen.split(",") : [],
+          type_vente: catalogue.type_vente.length
+            ? catalogue.type_vente.split(",")
+            : [],
+          produit_achete: catalogue.produit_achete.length
+            ? catalogue.produit_achete.split(",")
+            : [],
+          distribution: catalogue.distribution.length
+            ? catalogue.distribution.split(",")
+            : [],
+        };
       }
     },
     setIdentite: (state, action) => {
-      state.identite = action.payload
+      state.identite = action.payload;
     },
     setMarque: (state, action) => {
-      state.marque = action.payload
+      state.marque = action.payload;
     },
     setCatalogue: (state, action) => {
-      state.catalogue = action.payload
+      state.catalogue = action.payload;
     },
     setArticles: (state, action) => {
-      state.articles = action.payload
+      state.articles = action.payload;
     },
     setArticle: (state, action) => {
-      state.article = action.payload
+      state.article = action.payload;
     },
     setCible: (state, action) => {
-      state.cible = action.payload
+      state.cible = action.payload;
     },
     setRegister: (state, action) => {
-      state.register = action.payload
+      state.register = action.payload;
     },
     setReferences: (state, action) => {
-      state.references = action.payload
-    }
-  }
-})
+      state.references = action.payload;
+    },
+  },
+});
 
-export const { setIdentite, setMarque, setCatalogue, setCible, setRegister, setArticle, setArticles, setReferences, setProfil } = profileSlice.actions
-export const profileReducer = profileSlice.reducer
+export const {
+  setIdentite,
+  setMarque,
+  setCatalogue,
+  setCible,
+  setRegister,
+  setArticle,
+  setArticles,
+  setReferences,
+  setProfil,
+} = profileSlice.actions;
+export const profileReducer = profileSlice.reducer;
--- src/views/signup/index.js
+++ src/views/signup/index.js
@@ -1,43 +1,52 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
-import {setRegister} from "../../store/profileSlice";
-import {checkAuth, register} from "../../lib/auth";
-import { useSnackbar } from 'react-simple-snackbar'
+import { useDispatch, useSelector } from "react-redux";
+import { setRegister } from "../../store/profileSlice";
+import { checkAuth, register } from "../../lib/auth";
+import { useSnackbar } from "react-simple-snackbar";
 import snackbarStyles from "../../lib/snackbarStyles";
+import "./styles.scss";
 
 const Signup = () => {
-  const [openSnackbar, closeSnackbar] = useSnackbar(snackbarStyles)
-  const dispatch = useDispatch()
-  const registerForm = useSelector((state) => state.profile.register)
+  const [dr, setDr] = useState("");
+  const [openSnackbar, closeSnackbar] = useSnackbar(snackbarStyles);
+  const dispatch = useDispatch();
+  const registerForm = useSelector((state) => state.profile.register);
 
   const navigate = useNavigate();
 
   useEffect(() => {
-    const token = localStorage.getItem('token')
-    checkAuth(token)
-      .then(() => {
-        navigate('/company-setting')
-      })
-  }, [])
+    const token = localStorage.getItem("token");
+    checkAuth(token).then(() => {
+      navigate("/company-setting");
+    });
+  }, []);
   const handleInputUpdate = (field, e) => {
-    let data = { ...registerForm }
-    data[field] = e.target.value
-    dispatch(setRegister(data))
-  }
+    setDr("Directeur ");
+    let data = { ...registerForm };
+    data[field] = e.target.value;
+    dispatch(setRegister(data));
+  };
+  const handleAgreementConditions = (e) => {
+    let data = { ...registerForm };
+    data.terms = e.target.checked;
+    dispatch(setRegister(data));
+  };
   const handleClick = async () => {
-    try {
-      await register(registerForm)
-      navigate('/login')
-    }catch(e) {
-      let data = e.response.data
-      openSnackbar(
-        <ul>
-          {
-            Object.values(data.errors).map((errors) => errors.map((error) => <li>{error}</li>))
-          }
-        </ul>
-      )
+    if (registerForm.terms) {
+      try {
+        await register(registerForm);
+        navigate("/login");
+      } catch (e) {
+        let data = e.response.data;
+        openSnackbar(
+          <ul>
+            {Object.values(data.errors).map((errors) =>
+              errors.map((error) => <li>{error}</li>)
+            )}
+          </ul>
+        );
+      }
     }
   };
 
@@ -45,134 +54,147 @@ const Signup = () => {
     <>
       <div className="background"></div>
 
-    <div className="container pt-5">
-      <div className="text-center">
-        <h1 className="text-secondary">Accédez au marché en temps réel!</h1>
-        <p className="text-primary">
-          Ne ratez pas aucune opportunité d'affaires! Créez votre compte dès
-          aujourd'hui!
-        </p>
-      </div>
-      <div className="row mt-4">
-        <div className="col-md-6 d-flex justify-content-center align-items-start">
-          {/*<img src="https://via.placeholder.com/300" alt="" />*/}
-          <img src="/imgs/business-bag.png" alt="" width={400}/>
+      <div className="container pt-5">
+        <div className="text-center">
+          <h1 className="text-secondary">Accédez au marché en temps réel!</h1>
+          <p className="text-primary">
+            Ne ratez pas aucune opportunité d'affaires! Créez votre compte dès
+            aujourd'hui!
+          </p>
         </div>
-        <div className="col-md-6">
-          <div className="form-group mb-4">
-            <select
-              name="titre"
-              className="form-control rounded-pill border-0"
-              placeholder="Titre"
-              defaultValue={registerForm.titre}
-              onChange={(e) => handleInputUpdate('titre', e)}
-            >
-              <option value="m">M</option>
-              <option value="mme">Mme</option>
-              <option value="dr">Dr</option>
-              <option value="pr">Pr</option>
-            </select>
+        <div className="row mt-4">
+          <div className="col-md-6 d-flex justify-content-center align-items-start">
+            {/*<img src="https://via.placeholder.com/300" alt="" />*/}
+            <img src="/imgs/business-bag.png" alt="" width={400} />
           </div>
-          <div className="row mb-4">
-            <div className="col-6">
-              <div className="form-group">
-                <input
-                  type="text"
-                  className="form-control rounded-pill border-0"
-                  placeholder="Prénom"
-                  defaultValue={registerForm.prenom}
-                  onChange={(e) => handleInputUpdate('prenom', e)}
-                />
+          <div className="col-md-6">
+            <div className="form-group mb-4">
+              <select
+                name="titre"
+                className="form-control rounded-pill border-0"
+                placeholder="Titre"
+                defaultValue={registerForm.titre}
+                onChange={(e) => handleInputUpdate("titre", e)}
+              >
+                <option value="m">M</option>
+                <option value="mme">Mme</option>
+                <option value="dr">Dr</option>
+                <option value="pr">Pr</option>
+              </select>
+            </div>
+            <div className="row mb-4">
+              <div className="col-6">
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control rounded-pill border-0"
+                    placeholder="Prénom"
+                    defaultValue={registerForm.prenom}
+                    onChange={(e) => handleInputUpdate("prenom", e)}
+                  />
+                </div>
+              </div>
+              <div className="col-6">
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control rounded-pill border-0"
+                    placeholder="Nom"
+                    defaultValue={registerForm.nom}
+                    onChange={(e) => handleInputUpdate("nom", e)}
+                  />
+                </div>
               </div>
             </div>
-            <div className="col-6">
-              <div className="form-group">
-                <input
-                  type="text"
-                  className="form-control rounded-pill border-0"
-                  placeholder="Nom"
-                  defaultValue={registerForm.nom}
-                  onChange={(e) => handleInputUpdate('nom', e)}
-                />
-              </div>
+            <div className="form-group mb-4">
+              <select
+                name="poste"
+                className="form-control rounded-pill border-0"
+                placeholder="Poste Occupé"
+                defaultValue={registerForm.poste}
+                onChange={(e) => handleInputUpdate("poste", e)}
+              >
+                <option value="directeur_general">Directeur</option>
+                <option value="directeur_commercial">{dr}Commercial</option>
+                <option value="directeur_marketing">{dr}Marketing</option>
+                <option value="directeur_finance">{dr}Finance</option>
+                <option value="directeur_achat">{dr}Achat</option>
+                <option value="directeur_communication">
+                  {dr}Communication
+                </option>
+                <option value="directeur_informatique">{dr}Informatique</option>
+                <option value="directeur_ressource_humaine">
+                  {dr}Ressource humaine
+                </option>
+                <option value="directeur_technique">Technique</option>
+              </select>
             </div>
-          </div>
-          <div className="form-group mb-4">
-            <select
-              name="poste"
-              className="form-control rounded-pill border-0"
-              placeholder="Poste Occupé"
-              defaultValue={registerForm.poste}
-              onChange={(e) => handleInputUpdate('poste', e)}
-            >
-              <option value="directeur_general">Directeur général</option>
-              <option value="directeur_commercial">Directeur Commercial</option>
-              <option value="directeur_marketing">Directeur Marketing</option>
-              <option value="directeur_finance">Directeur Finance</option>
-              <option value="directeur_achat">Directeur Achat</option>
-              <option value="directeur_communication">
-                Directeur Communication
-              </option>
-              <option value="directeur_informatique">
-                Directeur Informatique
-              </option>
-              <option value="directeur_ressource_humaine">
-                Directeur Ressource humaine
-              </option>
-              <option value="directeur_technique">Directeur Technique</option>
-            </select>
-          </div>
-          <div className="form-group mb-4">
-            <input
-              type="text"
-              className="form-control rounded-pill border-0"
-              placeholder="E-mail"
-              defaultValue={registerForm.email}
-              onChange={(e) => handleInputUpdate('email', e)}
-            />
-          </div>
-          <div className="form-group mb-4">
-            <input
-              type="text"
-              className="form-control rounded-pill border-0"
-              placeholder="Téléphone"
-              defaultValue={registerForm.phone}
-              onChange={(e) => handleInputUpdate('phone', e)}
-            />
-          </div>
-          <div className="form-group mb-4">
-            <input
-              type="password"
-              className="form-control rounded-pill border-0"
-              placeholder="Mot de passe"
-              defaultValue={registerForm.password}
-              onChange={(e) => handleInputUpdate('password', e)}
-            />
-          </div>
-          <div className="form-group mb-4">
-            <input
-              type="password"
-              className="form-control rounded-pill border-0"
-              placeholder="Confirmer votre mot de passe"
-              defaultValue={registerForm.password_confirmation}
-              onChange={(e) => handleInputUpdate('password_confirmation', e)}
-            />
-          </div>
-          <div className="d-flex flex-column align-items-end mb-5">
-            <p className="small">
-              En cliquant sur s'inscrire, vous acceptez nos{" "}
-              <a href="#">Conditions d'utilisation</a>
-            </p>
-            <button
-              onClick={handleClick}
-              className="btn btn-success text-white rounded-pill px-4"
-            >
-              Envoyer
-            </button>
+            <div className="form-group mb-4">
+              <input
+                type="text"
+                className="form-control rounded-pill border-0"
+                placeholder="E-mail"
+                defaultValue={registerForm.email}
+                onChange={(e) => handleInputUpdate("email", e)}
+              />
+            </div>
+            <div className="form-group mb-4">
+              <input
+                type="text"
+                className="form-control rounded-pill border-0"
+                placeholder="Téléphone"
+                defaultValue={registerForm.phone}
+                onChange={(e) => handleInputUpdate("phone", e)}
+              />
+            </div>
+            <div className="form-group mb-4">
+              <input
+                type="password"
+                className="form-control rounded-pill border-0"
+                placeholder="Mot de passe"
+                defaultValue={registerForm.password}
+                onChange={(e) => handleInputUpdate("password", e)}
+              />
+            </div>
+            <div className="form-group mb-4">
+              <input
+                type="password"
+                className="form-control rounded-pill border-0"
+                placeholder="Confirmer votre mot de passe"
+                defaultValue={registerForm.password_confirmation}
+                onChange={(e) => handleInputUpdate("password_confirmation", e)}
+              />
+            </div>
+            <div className="d-flex flex-column align-items-start mb-5">
+              Conformément à la loi 09-08, vous disposez d'un droit d'accès, de
+              rectifications et d'opposition au traitement de vos données
+              personnelles. Ce traitement a été autorisé par la CNDP sous le
+              n°XXXXX
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="flexCheckDefault"
+                  onChange={(e) => handleAgreementConditions(e)}
+                  checked={registerForm.terms}
+                />
+                <label className="form-check-label" for="flexCheckDefault">
+                  J'ai lu et j'accepte les conditions générales d'utilisation,
+                  notamment la mention relative à la protection des données
+                  personnelles
+                </label>
+              </div>
+              <button
+                onClick={handleClick}
+                className="btn btn-success text-white rounded-pill m-20 signIn "
+              >
+                Envoyer
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
