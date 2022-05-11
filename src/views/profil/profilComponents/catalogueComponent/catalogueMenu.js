cece801f0abca5024268cb64a11d6b9bc3b28147
amine <abdelmoumen33330@gmail.com>
2022-05-11 16:10:28 +0100

    more styling

--- src/views/profil/profilComponents/catalogueComponent/catalogueMenu.js
+++ src/views/profil/profilComponents/catalogueComponent/catalogueMenu.js
@@ -25,17 +25,33 @@ export default function CatalogueMenu() {
 
         <div className="col-12  col-md-3 mt-3 me-2 ">
           <div
-            className="d-flex justify-content-center cursor-pointer rounded-pill py-1 bg-success text-white "
+            className={
+              productComponent == 1
+                ? "d-flex justify-content-center cursor-pointer rounded-pill py-1 bg-success text-white "
+                : "d-flex justify-content-center btn  btn-outline-primary rounded-pill py-1 bg-white text-primary"
+            }
             onClick={() => setProductComponent(1)}
           >
-            <img style={{ width: "25px" }} src="/imgs/product.svg" alt="" />
+            <img
+              style={{ width: "25px" }}
+              src={
+                productComponent == 1
+                  ? "/imgs/product.svg"
+                  : "imgs/product12.png"
+              }
+              alt=""
+            />
             <span className="catalogue-text">Product</span>
           </div>
         </div>
         <div className="col-12  col-md-3 mt-3 me-2 ">
           <div
             onClick={() => setProductComponent(2)}
-            className="d-flex justify-content-center btn  btn-outline-primary rounded-pill py-1 bg-white text-primary"
+            className={
+              productComponent == 2
+                ? "d-flex justify-content-center cursor-pointer rounded-pill py-1 bg-success text-white "
+                : "d-flex justify-content-center btn  btn-outline-primary rounded-pill py-1 bg-white text-primary"
+            }
           >
             <img style={{ width: "25px" }} src="/imgs/service.svg" alt="" />
             <span className="catalogue-text">Service</span>
@@ -44,7 +60,11 @@ export default function CatalogueMenu() {
         <div className="col-12  col-md-3 mt-3 me-2 ">
           <div
             onClick={() => setProductComponent(3)}
-            className="d-flex justify-content-center btn  btn-outline-primary rounded-pill py-1 bg-white text-primary"
+            className={
+              productComponent == 3
+                ? "d-flex justify-content-center cursor-pointer rounded-pill py-1 bg-success text-white "
+                : "d-flex justify-content-center btn  btn-outline-primary rounded-pill py-1 bg-white text-primary"
+            }
           >
             <img style={{ width: "25px" }} src="/imgs/house.svg" alt="" />
             <span className="catalogue-text">Bien Immobilier</span>
